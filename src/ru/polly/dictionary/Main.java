dd8cc2d56759edd9cecce7631e717728438f52dc
lollypolly <polina.mokeeva@mail.ru>
2020-04-20 14:22:08 +0300

    19/04/2020

--- src/ru/polly/dictionary/Main.java
+++ src/ru/polly/dictionary/Main.java
@@ -6,11 +6,22 @@ public class Main {
     public static void main(String[] args) {
         try {
             Dictionary dictionary = new Dictionary("C:\\Users\\Полин о4чка\\Desktop\\dict.txt");
+            System.out.println(" Просмотр словаря:");
             dictionary.show();
             System.out.println();
+            dictionary.insert("ability","способность, возможность");
+            System.out.println(" Просмотр словаря с добавлением нового слова:");
+            dictionary.show();
+            dictionary.delete("ability");
+            System.out.println();
+            System.out.println(" Просмотр словаря с удалением добавленного нового слова:");
+            dictionary.show();
+            System.out.println();
+            System.out.println(" Просмотр уникальных слов:");
             System.out.println(dictionary.unique());
             System.out.println();
-            System.out.println(dictionary.translate("polly apple"));
+            System.out.println(" Перевод текста:");
+            System.out.println(dictionary.translate("hello everyone this my semester work polly"));
         } catch (IOException e) {
             e.printStackTrace();
         }
